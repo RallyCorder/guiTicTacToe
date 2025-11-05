84437ca6ecdda7ce3ef66becef1695341c877cfa
Rally <gg64mail@protonmail>
2025-11-05 12:42:02 +0100

    made button interactions appropriate to curplayer

--- nightly.py
+++ nightly.py
@@ -22,55 +22,15 @@ class Case:
         else:
             return str(self.position)
 
-
-class Grille:
-
-    def __init__(self):
-        self.grille= [Case(0,None),Case(1,None),Case(2,None),Case(3,None),Case(4,None),Case(5,None),Case(6,None),Case(7,None),Case(8,None)]
-
-    def estVide(self,pos):
-        return self.grille[pos].valeur==None
-
-    def joue(self,pos,perso):
-        self.grille[pos].valeur= perso.symbole
-
-    def verif_victoire(self):
-        for i in range(0,9,3):
-            if self.grille[0+i].valeur == self.grille[1+i].valeur == self.grille[2+i].valeur and self.grille[0+i].valeur != None:
-                return True
-        for i in range(3):
-            if self.grille[0+i].valeur == self.grille[3+i].valeur == self.grille[6+i].valeur and self.grille[0+i].valeur != None:
-                return True
-        if self.grille[0].valeur == self.grille[4].valeur == self.grille[8].valeur and self.grille[0].valeur != None:
-                return True
-        if self.grille[2].valeur == self.grille[4].valeur == self.grille[6].valeur and self.grille[2].valeur != None:
-                return True
-
-
-
-    def __str__(self):
-        text=""
-        for j in range(0,9,3):
-            text+="|"
-            for i in range (0,3):
-                if self.estVide(j+i):
-                    a=" "
-                else:
-                    a= self.grille[j+i]
-                text+=str(a)+"|"
-            text+="\n"
-
-        return text
-
 class GUI(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         
         self.hello = ["X","O"]
         self.listeJoueurs =  [Joueur(input("Nom joueur 1:"),"X"),Joueur(input("Nom joueur 2:"),"O")]
-        self.plateau = Grille()
         self.joueurActu = 1
         self.compteur = 0
+        self.casechoisie = None
 
         self.button1 = QtWidgets.QPushButton("")
         self.button2 = QtWidgets.QPushButton("")
@@ -92,32 +52,17 @@ class GUI(QtWidgets.QWidget):
         self.layout.addWidget(self.button7, 2, 0)
         self.layout.addWidget(self.button8, 2, 1)
         self.layout.addWidget(self.button9, 2, 2)
-        self.button1.clicked.connect(self.round)
-        self.button2.clicked.connect(self.magic)
-        self.button3.clicked.connect(self.magic)
-        self.button4.clicked.connect(self.magic)
-        self.button5.clicked.connect(self.magic)
-        self.button6.clicked.connect(self.magic)
-        self.button7.clicked.connect(self.magic)
-        self.button8.clicked.connect(self.magic)
-        self.button9.clicked.connect(self.magic)
 
+        self.button1.clicked.connect(self.input1)
+        self.button2.clicked.connect(self.input2)
+        self.button3.clicked.connect(self.input3)
+        self.button4.clicked.connect(self.input4)
+        self.button5.clicked.connect(self.input5)
+        self.button6.clicked.connect(self.input6)
+        self.button7.clicked.connect(self.input7)
+        self.button8.clicked.connect(self.input8)
+        self.button9.clicked.connect(self.input9)
 
-    @QtCore.Slot()
-    def magic(self):
-        self.button1.setText(GUI.tours.casechoisie(self.hello))
-        self.button2.setText(random.choice(self.hello))
-        self.button3.setText(random.choice(self.hello))
-        self.button4.setText(random.choice(self.hello))
-        self.button5.setText(random.choice(self.hello))
-        self.button6.setText(random.choice(self.hello))
-        self.button7.setText(random.choice(self.hello))
-        self.button8.setText(random.choice(self.hello))
-        self.button9.setText(random.choice(self.hello))
-
-    def joueur_actuel(self):
-        Player= self.listeJoueurs[self.joueurActu]
-        return Player
 
 
     def SwitchPlayer(self):
@@ -126,15 +71,64 @@ class GUI(QtWidgets.QWidget):
         elif self.joueurActu == 0:
             self.joueurActu = 1
 
-
-    def tours(self):
-        print(self.plateau)
-        casechoisie= GUI.self.QtWidgets("Ecrit un nombre entre 1 et 9 ")
-        while self.plateau.estVide(int(casechoisie)-1)!=True:
-            print("La case choisie est déjà prise, choisissez une autre case")
-            casechoisie = input("Ecrit un nombre entre 1 et 9 ") #peut rajouter une condition pour verifier que le joueur met bien un numéro entre 1 et 9
-        self.plateau.joue(int(casechoisie)-1,self.joueur_actuel())
+    @QtCore.Slot()
+    def input1(self):
+        casechoisie = self.joueur_actuel()
         self.compteur+=1
+        self.button1.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input2(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button2.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input3(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button3.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input4(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button4.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input5(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button5.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input6(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button6.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input7(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button7.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input8(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button8setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def input9(self):
+        casechoisie = self.joueur_actuel()
+        self.compteur+=1
+        self.button9.setText(str(casechoisie))
+        self.SwitchPlayer()
+
+    def joueur_actuel(self):
+        Player= self.listeJoueurs[self.joueurActu]
+        return Player
 
     def jeu_entier(self):
         while self.plateau.verif_victoire()!=True and self.compteur!=9:
