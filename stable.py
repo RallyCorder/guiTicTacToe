011b295b27cad97318cc76775867ec9b9bcc8add
Rally <gg64mail@protonmail>
2025-11-21 11:36:41 +0100

    pushing a stable release, working on separating classes in nightly

--- stable.py
+++ stable.py
@@ -3,157 +3,40 @@ import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
 class Player:
-    def __init__(self,username,symbol):
-        self.username=username
-        self.symbol=symbol
+    def __init__(self,nom,symbole):
+        self.nom=nom
+        self.symbole=symbole
 
     def __str__(self):
-        return self.username+ ": "+self.symbol
+        return self.nom+ ": "+self.symbole
 
-class Gameloop:
+class Box:
 
-    def __init__(self):
-        self.listeJoueurs=[Player(input("Player Ж is called: "),"Ж"),Player(input("Player 𝛀 is called: "),"𝛀")]
-        self.joueurActu=random.randint(0,1)
-        self.chosen_box=None
-        for i in range(1,10):
-            setattr(self,"dot"+str(i),0)
-        
+    def __init__(self,position,valeur):
+        self.position=position
+        self.valeur=valeur
 
-    def user_input1(self):
-            GUI.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button1.setText(str(self.current_player))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            GUI.button1.setEnabled(False)
-
-    def user_input2(self):
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button1.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            GUI.button2.setEnabled(False)
-
-    def user_input2(self):
-        if self.dot2==1:
-            pass
+    def __str__(self):
+        if self.valeur!= None:
+            return self.valeur
         else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button2.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot2+=1
-
-    def user_input3(self):
-        if self.dot3==1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button3.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot3+=1
-
-    def user_input4(self):
-        if self.dot4==1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button4.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot4+=1
-
-    def user_input5(self):
-        if self.dot5 == 1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button5.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot5+=1
-
-    def user_input6(self):
-        if self.dot6 == 1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button6.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot6+=1
-
-    def user_input7(self):
-        if self.dot7 == 1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button7.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot7+=1
-
-    def user_input8(self):
-        if self.dot8 == 1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button8.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot8+=1
-
-    def user_input9(self):
-        if self.dot9 == 1:
-            pass
-        else:
-            chosen_box = self.current_player()
-            self.counter+=1
-            GUI.countertext.setText(str(self.counter))
-            GUI.button9.setText(str(chosen_box))
-            self.SwitchPlayer()
-            GUI.Wincheck()
-            self.dot9+=1
-
-    def SwitchPlayer(self):
-        if self.joueurActu == 1:
-            self.joueurActu = 0
-        elif self.joueurActu == 0:
-            self.joueurActu = 1  
-
-    def current_player(self):
-        Player_current= self.listeJoueurs[self.joueurActu]
-        return Player_current      
-
-    def Game(self):
-        self.listeJoueurs
+            return str(self.position)
 
 class GUI(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        
+        self.listeJoueurs=[Player(input("Player Ж is called: "),"Ж"),Player(input("Player 𝛀 is called: "),"𝛀")]
+        self.joueurActu=random.randint(0,1)
+        self.counter=0
+        self.chosen_box=None
+        for i in range(1,10):
+            setattr(self, "dot"+str(i),0)
+
 
         for i in range(1,10):
             setattr(self,"button"+str(i),QtWidgets.QPushButton(None))
         self.wintext=QtWidgets.QLabel("")
-        self.counter=0
         self.countertext=QtWidgets.QLabel(str(self.counter))
         
         self.layout=QtWidgets.QGridLayout(self)
@@ -164,8 +47,123 @@ class GUI(QtWidgets.QWidget):
                 self.layout.addWidget(getattr(self, "button"+str(nb)),i,j)
         self.layout.addWidget(self.wintext,0,2)
         self.layout.addWidget(self.countertext,1,2)
+
         for i in range(1,10):
-            getattr(self,"button"+str(i)).clicked.connect(getattr(Gameloop,"user_input"+str(i)))
+            getattr(self, "button"+str(i)).clicked.connect(getattr(self,"user_input"+str(i)))
+
+    def user_input1(self):
+        if self.dot1==1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button1.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot1+=1
+
+    def user_input2(self):
+        if self.dot2==1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button2.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot2+=1
+
+    def user_input3(self):
+        if self.dot3==1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button3.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot3+=1
+
+    def user_input4(self):
+        if self.dot4==1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button4.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot4+=1
+
+    def user_input5(self):
+        if self.dot5 == 1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button5.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot5+=1
+
+    def user_input6(self):
+        if self.dot6 == 1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button6.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot6+=1
+
+    def user_input7(self):
+        if self.dot7 == 1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button7.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot7+=1
+
+    def user_input8(self):
+        if self.dot8 == 1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button8.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot8+=1
+
+    def user_input9(self):
+        if self.dot9 == 1:
+            pass
+        else:
+            chosen_box = self.current_player()
+            self.counter+=1
+            self.countertext.setText(str(self.counter))
+            self.button9.setText(str(chosen_box))
+            self.SwitchPlayer()
+            self.Wincheck()
+            self.dot9+=1
+
+    def SwitchPlayer(self):
+        if self.joueurActu == 1:
+            self.joueurActu = 0
+        elif self.joueurActu == 0:
+            self.joueurActu = 1
 
     def Wincheck(self):
         for i in range(1,8,3):
@@ -192,8 +190,19 @@ class GUI(QtWidgets.QWidget):
                 pass
             else:
                 self.wintext.setText(str(self.button3.text())+" is the winner!")
+            
+    def current_player(self):
+        Player_current= self.listeJoueurs[self.joueurActu]
+        return Player_current
 
-Gameloop()
+    def jeu_entier(self):
+        while self.plateau.verif_victoire()!=True and self.counter!=9:
+            self.SwitchPlayer()
+            self.tours()
+        if self.plateau.verif_victoire():
+            print(f"Bravo ! "+self.current_player().nom+" vous avez gagné ! ")
+        else:
+            print("Egalité")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
@@ -203,6 +212,9 @@ if __name__ == "__main__":
     widget.show()
 
     sys.exit(app.exec())
+
+j=GUI()
+j.jeu_entier()
 
 ''' attempt at optimising the user_input func, failure due to line 50 getattr(self, "user_input"+str(i)), 'GUI' object has no attribute 'user_input1'
     def user_input(self, i):
